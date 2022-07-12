f4b3e019bbb88ace9f3861d4b2d1449b26927125
Boccarusso <gabrieleboccarusso@gmail.com>
2022-07-12 22:47:10 +0200

    .

--- controller.js
+++ controller.js
@@ -3,8 +3,10 @@
 
     // let baseURL = "https://boccarussofrontend.herokuapp.com/";
     let baseURL = window.location.href;
+    const splitted = baseURL.split('/');
     const style = document.getElementsByTagName("head")[0];
-    console.log(baseURL.split('/'));
+    const main = document.getElementsByClassName('inner')[0];
+    main.innerHTML += splitted[splitted.length-1];
 
     style.innerHTML += "<title>TITLE</title>";
 })();
