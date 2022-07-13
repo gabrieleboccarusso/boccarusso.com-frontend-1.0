cb0831aff05c107fee1e54be87ac56dc388a2e6f
Boccarusso <gabrieleboccarusso@gmail.com>
2022-07-13 12:42:32 +0200

    completed homepage api request

--- api.js
+++ api.js
@@ -3,31 +3,45 @@ function homePosts() {
     const spinnerBox = document.getElementById('spinner-box');
     const loadBtn = document.getElementById('load-btn');
 
-    let max = 5;
-    let end = max;
-    let begin = max-2;
+    let switchFunc = "";
+    let max;
+    let end;
+    let begin;
 
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            if((end -3)<= 0) { loadBtn.remove(); }
-            begin -= 3; end -= 3;
-            const posts = JSON.parse(this.responseText);
-            posts.forEach(appendArticles);
+            switch(switchFunc) {
+                case "getMax":
+                    max = this.responseText;
+                    end = parseInt(max);
+                    begin = end-2;
+                    getThreeLastPost();
+                    break;
+                case "getThreeLastPost":
+                    if((end -3)<= 0) { loadBtn.remove(); }
+                    begin -= 3; end -= 3;
+                    const posts = JSON.parse(this.responseText);
+                    posts.forEach(appendArticles);
+                    break;
+            }
           }
     };
     
-    
-    function getThreeLastPost() {
-        api = "https://boccarussoapi.herokuapp.com/posts/between/"+begin+"/"+end;
-        console.log(api);
+    function getMax() {
+        switchFunc = "getMax";
+        let api = "https://boccarussoapi.herokuapp.com/posts/maxId";
         xmlhttp.open("GET", api, true);
-        
+        xmlhttp.send();
+    } getMax();
+
+    function getThreeLastPost() {
+        switchFunc = "getThreeLastPost";
+        let api = "https://boccarussoapi.herokuapp.com/posts/between/"+begin+"/"+end;
+        xmlhttp.open("GET", api, true);
         xmlhttp.send();
     }
-    
-    getThreeLastPost();
-    
+
     loadBtn.addEventListener('click', () => {
         getThreeLastPost();
     })
